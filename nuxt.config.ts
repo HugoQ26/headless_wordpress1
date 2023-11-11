7c61442092db7f5c46eeacab644072126e4fb122
hugoQ26 <mkubiak73@gmail.com>
2023-11-11 09:01:39 +0100

    inital vue

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['nuxt-graphql-client'],
+})
